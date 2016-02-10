a9c1573941442b429a3ef54841f76ead402ffaf8
The Alchemist <kap4020@gmail.com>
2016-02-10 14:35:10 -0500

    JmsResourceCandidateResolver

---
+++ JmsResourceCandidateResolver.java
@@ -0,0 +1,67 @@
+
+import java.lang.annotation.Annotation;
+
+import javax.annotation.Resource;
+import javax.jms.Queue;
+import javax.jms.Topic;
+
+import org.springframework.beans.factory.config.DependencyDescriptor;
+import org.springframework.beans.factory.support.SimpleAutowireCandidateResolver;
+import org.springframework.core.annotation.AnnotationUtils;
+
+import com.mockrunner.jms.DestinationManager;
+
+public class JmsResourceCandidateResolver extends SimpleAutowireCandidateResolver {
+
+    private final DestinationManager mockJmsDestinationManager;
+
+    public JmsResourceCandidateResolver(DestinationManager mockJmsDestinationManager) {
+        this.mockJmsDestinationManager = mockJmsDestinationManager;
+    }
+
+    @Override
+    public Object getSuggestedValue(DependencyDescriptor descriptor) {
+        String dependencyClassName = descriptor.getDependencyType().getName();
+        if(descriptor.getField() != null) {
+            Annotation[] annotations = descriptor.getAnnotations();
+            // support Queue objects
+            if (dependencyClassName.equals(Queue.class.getName())) {
+                String queueName = getJmsQueueName(annotations);
+                if(queueName == null) {
+                    throwException(dependencyClassName);
+                } else {
+                    return mockJmsDestinationManager.createQueue(queueName);
+                }
+            }
+            // and Topic objects
+            else if(dependencyClassName.equals(Topic.class.getName())) {
+                String queueName = getJmsQueueName(annotations);
+                if(queueName == null) {
+                    throwException(dependencyClassName);
+                } else {
+                    return mockJmsDestinationManager.createTopic(queueName);
+                }
+            }
+        }
+
+        return super.getSuggestedValue(descriptor);
+    }
+
+
+    private void throwException(String className) {
+        throw new IllegalStateException(String.format("Missing @Resource(lookup=...) annotation; Cannot inject a %s because I do not know what name to give it", className));
+    }
+
+    private String getJmsQueueName(Annotation[] annotations) {
+        for (Annotation ann : annotations) {
+            Resource resource = AnnotationUtils.getAnnotation(ann, Resource.class);
+            if(resource.name() == null) {
+                throw new IllegalStateException("Could not inject a Queue because the @Resource annotation was missing a @Resource(lookup=....)");
+            } else {
+                return resource.name();
+            }
+        }
+        return null;
+    }
+
+}
